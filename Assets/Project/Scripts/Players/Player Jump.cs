a3c8a4710b858b5ae0407b14d136bcc5173e7dee
JannaAllahOmar95 <jannaallah95@gmail.com>
2024-05-15 08:17:14 -0700

    Jump using Kinematic Function
    Jump logic is using the velocity equation, and i can control the jump height and gravity from the editor.

--- Assets/Project/Scripts/Players/Player Jump.cs
+++ Assets/Project/Scripts/Players/Player Jump.cs
@@ -4,28 +4,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
-    #region Parameters
-
     [Header("Jump")]
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpHeight = 1f;
+    [SerializeField] private float jumpDuration = 1f;
 
-    [SerializeField] private AnimationCurve jumpCurve;
-    [SerializeField] private int jumpCount = 0;
+    [Header("Physics")]
+    [SerializeField] private float gravity = 20f;
+    [SerializeField] private float maxVelocity = 10f;
+    [SerializeField] private float maxForce = 20f;
+
+    private float VelocityNode;
+
+    private int jumpCount = 0;
 
     private InputManager _inputManager;
-
     private Action<InputAction.CallbackContext> _jumpAction;
-
     private Rigidbody rb;
-
-    #endregion Parameters
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         _inputManager = ServiceLocator.Instance.GetService<InputManager>();
-
         _jumpAction = _ => Jump();
         _inputManager.PlayerActions.Jump.performed += _jumpAction;
     }
@@ -36,11 +36,32 @@ public class PlayerJump : MonoBehaviour
     {
         if (jumpCount < 2)
         {
-            float normalizedTime = Mathf.Clamp01(jumpCurve.keys[jumpCurve.length - 1].time);
-            float evaluatedJumpForce = jumpCurve.Evaluate(Time.time / normalizedTime) * jumpForce;
-
-            rb.AddForce(Vector3.up * evaluatedJumpForce, ForceMode.Impulse);
+            VelocityNode = (2 * gravity * jumpHeight) / jumpDuration;
+            rb.velocity = new Vector3(rb.velocity.x, VelocityNode, rb.velocity.z);
             jumpCount++;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyGravity();
+        LimitVelocity();
+    }
+
+    private void ApplyGravity()
+    {
+        rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+    }
+
+    private void LimitVelocity()
+    {
+        if (rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = rb.velocity.normalized * maxVelocity;
+        }
+        if (rb.velocity.magnitude > maxForce)
+        {
+            rb.velocity = rb.velocity.normalized * maxForce;
         }
     }
 
