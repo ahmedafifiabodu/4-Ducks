c6575475d320af2391f0f59c18bc39561aa8d177
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-05-29 00:07:28 +0300

    - Updating Enemy AI

---
+++ Assets/Project/Scripts/Enemy AI/Navmesh Agent/Enemy Spawner (Type).cs
@@ -0,0 +1,10 @@
+using System;
+using Unity.AI.Navigation;
+using UnityEngine;
+
+[Serializable]
+public class EnemySpawnerType
+{
+    public GameObject _enemiesPrefabs;
+    public NavMeshSurface _navMeshSurface;
+}
