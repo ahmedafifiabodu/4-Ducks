548f20d5f77bf9d6f671f1d21c4f210558cb58d4
1Rooky <rokaiamedhat1@gmail.com>
2024-05-29 09:35:45 +0300

    Room 1 Integrated

--- Assets/Project/Scripts/System/UI System/UI_Bars/EnergyBar.cs
+++ Assets/Project/Scripts/System/UI System/UI_Bars/EnergyBar.cs
@@ -8,14 +8,21 @@ public class EnergyBar : MonoBehaviour
 
     private void OnEnable()
     {
+        Logging.Log(_energySystem.OnEnergyChanged);
         _energySystem.OnEnergyChanged.AddListener(UpdateEnergyBar);
         _image.fillAmount = _energySystem.EnergyPrecentage;
     }
-
+    private void Start()
+    {
+        Logging.Log("Here 3");
+        _image.fillAmount = _energySystem.EnergyPrecentage;
+    }
     private void UpdateEnergyBar()
     {
+        Logging.Log("Here 1");
         if (_energySystem != null)
         {
+            Logging.Log("Here 2");
             _image.fillAmount = _energySystem.EnergyPrecentage;
         }
     }
