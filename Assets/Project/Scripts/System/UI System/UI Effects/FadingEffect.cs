4c7b6aba2a7f2902431db2d0a37ee018dbe20e5c
1Rooky <rokaiamedhat1@gmail.com>
2024-06-03 10:45:34 +0300

    Fading Effect

--- Assets/Project/Scripts/System/UI System/UI Effects/FadingEffect.cs
+++ Assets/Project/Scripts/System/UI System/UI Effects/FadingEffect.cs
@@ -1,8 +1,3 @@
-using BehaviorDesigner.Runtime.Tasks.Unity.Timeline;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FadingEffect : MonoBehaviour
