e49469eb9e9bd857bfdc7a6b90a44555393403cc
1Rooky <rokaiamedhat1@gmail.com>
2024-06-04 17:53:30 +0300

    Folders reposition

--- Assets/Project/Scripts/Camera/CutOutObject.cs
+++ Assets/Project/Scripts/Camera/CutOutObject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CutOutObject : MonoBehaviour
 {
@@ -6,6 +7,11 @@ public class CutOutObject : MonoBehaviour
     [SerializeField] Transform _ghostTransform;
     [SerializeField] LayerMask _obstaclesMask;
     private Camera _camera;
+
+    private Material[] _cutoutMat;
+    private bool _needResetPos1;
+    private bool _needResetPos2;
+    RaycastHit[] __hitObjects;
     private void Start()
     {
         _camera = ServiceLocator.Instance.GetService<CameraInstance>().Camera;
@@ -18,23 +24,53 @@ public class CutOutObject : MonoBehaviour
         Vector3 _catOffest = _catTransform.position - transform.position;
         Vector3 _ghostOffest = _ghostTransform.position - transform.position;
 
+       
         RaycastHit[] _hitCatObjects = Physics.RaycastAll(transform.position, _catOffest, _catOffest.magnitude, _obstaclesMask);
         RaycastHit[] _hitGhostObjects = Physics.RaycastAll(transform.position, _ghostOffest, _ghostOffest.magnitude, _obstaclesMask);
 
-        for(int i =0; i< _hitCatObjects.Length; ++i)
+        if(_hitCatObjects.Length > 0)
         {
-            Material[] _materials  = _hitCatObjects[i].transform.GetComponent<Renderer>().materials;
-            for(int j = 0; j < _materials.Length;++j)
+            if(!_needResetPos1) _needResetPos1 = true;
+            SetMaterialPosition(_hitCatObjects, "_Player1Position", _cutOutCatPos);
+        }
+        if (_hitGhostObjects.Length > 0)
+        {
+            if (!_needResetPos1) _needResetPos2 = true;
+            SetMaterialPosition(_hitGhostObjects, "_Player2Position", _cutOutGhostPos);
+        }
+        // the point is _hit always saves the objects of ghost only try to use events to invoke it.
+        if (_needResetPos1)
+        {
+            ResetMaterialsPosition("_Player1Position");
+            _needResetPos1 = false;
+        }
+        if (_needResetPos2)
+        {
+            ResetMaterialsPosition("_Player2Position");
+            _needResetPos2 = false;
+        }
+    }
+    private void SetMaterialPosition(RaycastHit[] _hitObjects, string _matParamenter ,Vector2 _cutPos)
+    {
+
+        __hitObjects = _hitObjects;
+        for (int i = 0; i < _hitObjects.Length; ++i)
+        {
+            _cutoutMat = _hitObjects[i].transform.GetComponent<Renderer>().materials;
+            for (int j = 0; j < _cutoutMat.Length; ++j)
             {
-                _materials[j].SetVector("_Player1Position", _cutOutCatPos);
+                _cutoutMat[j].SetVector(_matParamenter, _cutPos);
             }
         }
-        for (int i = 0; i < _hitGhostObjects.Length; ++i)
+    }
+    private void ResetMaterialsPosition(string _matParamenter)
+    {
+        for (int i = 0; i < __hitObjects.Length; ++i)
         {
-            Material[] _materials = _hitGhostObjects[i].transform.GetComponent<Renderer>().materials;
-            for (int j = 0; j < _materials.Length; ++j)
+            _cutoutMat = __hitObjects[i].transform.GetComponent<Renderer>().materials;
+            for (int j = 0; j < _cutoutMat.Length; ++j)
             {
-                _materials[j].SetVector("_Player2Position", _cutOutGhostPos);
+                _cutoutMat[j].SetVector(_matParamenter, Vector3.zero);
             }
         }
     }
--- Assets/Project/Scripts/System/SpawningSystem/CheckPoint.cs
+++ Assets/Project/Scripts/System/SpawningSystem/CheckPoint.cs
@@ -6,26 +6,27 @@ using UnityEngine.Events;
 public class CheckPoint : Interactable, IDataPersistence
 {
     [SerializeField] private string _checkPointId;
-    private bool isPassed;
+    private bool _isPassed;
+    public bool IsPassed => _isPassed;
 
-    internal static UnityEvent<Transform> _onCheckPointPassed = new();
+    internal static UnityEvent<CheckPoint> _onCheckPointPassed = new();
     protected override void Interact(ObjectType _playerType)
     {
         base.Interact(_playerType);
-        isPassed = true;
-        _onCheckPointPassed?.Invoke(transform);
+        _isPassed = true;
+        _onCheckPointPassed?.Invoke(this);
     }
     [ContextMenu("Generate guid for ID")]
     private void GenerateID() => _checkPointId = System.Guid.NewGuid().ToString();
     public void LoadGame(GameData _gameData)
     { 
-        _gameData._checkPointPassed.TryGetValue(_checkPointId, out isPassed);
+        _gameData._checkPointPassed.TryGetValue(_checkPointId, out _isPassed);
     }
     public void SaveGame(GameData _gameData)
     {
         if (_gameData._checkPointPassed.ContainsKey(_checkPointId))
-            _gameData._checkPointPassed[_checkPointId] = isPassed;
+            _gameData._checkPointPassed[_checkPointId] = _isPassed;
         else
-            _gameData._checkPointPassed.Add(_checkPointId, isPassed);
+            _gameData._checkPointPassed.Add(_checkPointId, _isPassed);
     }
 }
