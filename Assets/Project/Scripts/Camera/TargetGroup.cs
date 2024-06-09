555675508a49033deb536f96ff8264fe2c391887
1Rooky <rokaiamedhat1@gmail.com>
2024-06-09 23:47:28 +0300

    Camera Scene Saved

--- Assets/Project/Scripts/Camera/TargetGroup.cs
+++ Assets/Project/Scripts/Camera/TargetGroup.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -7,23 +5,32 @@ public class TargetGroup : MonoBehaviour
 {
     CinemachineTargetGroup _targetGroup;
     ServiceLocator _serviceLocator;
-    [SerializeField] float _weight = 0.5f;
-    [SerializeField] float _radius = 1.0f;
+    [SerializeField] float _weight = 1.0f;
+    [SerializeField] float _radius = 0.5f;
     private void Awake()
     {
         _serviceLocator = ServiceLocator.Instance;
-        _serviceLocator.RegisterService<TargetGroup>(this, true);
+        _serviceLocator.RegisterService(this, true);
     }
     private void Start()
     {
-        _targetGroup.GetComponent<CinemachineTargetGroup>();
-        _targetGroup.Targets.Add(new CinemachineTargetGroup.Target { 
-            Object = _serviceLocator.GetService<Cat>().gameObject.transform,
-            Weight = _weight,
-            Radius = _radius});
-        _targetGroup.Targets.Add(new CinemachineTargetGroup.Target { 
-            Object = _serviceLocator.GetService<Ghost>().gameObject.transform,
-            Weight = _weight,
-            Radius = _radius});
+        _targetGroup = GetComponent<CinemachineTargetGroup>();
+        if (_targetGroup != null)
+        {
+            _targetGroup.Targets.Add(new CinemachineTargetGroup.Target
+            {
+                Object = _serviceLocator.GetService<Cat>().GetTransform(),
+                Weight = _weight,
+                Radius = _radius
+            });
+            _targetGroup.Targets.Add(new CinemachineTargetGroup.Target
+            {
+                Object = _serviceLocator.GetService<Ghost>().GetTransform(),
+                Weight = _weight,
+                Radius = _radius
+            });
+        }
+        else
+        { Logging.Log("haai i am null"); }
     }
 }
--- Assets/Project/Scripts/System/Interaction System/Object Type/Ghost/Ghost.cs
+++ Assets/Project/Scripts/System/Interaction System/Object Type/Ghost/Ghost.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class Ghost : MonoBehaviour
 {
     private void Awake() => ServiceLocator.Instance.RegisterService(this, true);
+    public Transform GetTransform() => transform;
 }
