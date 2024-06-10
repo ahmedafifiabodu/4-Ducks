93850fe3205c7921d3cc5dbfeaf8f62eb55f300d
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-10 21:19:31 +0300

    - Added new Feedback animation for the turret - Added Delay for each shoot - Fixed the bug of the ghost when possess

--- Assets/Project/Scripts/System/Interaction System/Object Type/Cat/Cat.cs
+++ Assets/Project/Scripts/System/Interaction System/Object Type/Cat/Cat.cs
@@ -5,7 +5,7 @@ public class Cat : MonoBehaviour
     [SerializeField] private AttackRadius _attackRadius;
     [SerializeField] private Animator _animator;
 
-    private void Awake() => ServiceLocator.Instance.RegisterService(this, true);
+    private void Awake() => ServiceLocator.Instance.RegisterService(this, false);
 
     public Transform GetTransform() => transform;
 }
