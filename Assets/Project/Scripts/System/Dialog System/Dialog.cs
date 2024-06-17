dee7c64a92043fdb48c2eb9745d2190b1df551af
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-18 01:46:09 +0300

    Refactor Dialog UI and Cleanup Assets
    - Updated DialogManager.cs to improve dialog handling and UI clarity. Changed namespace import from `Febucci.UI` to `Febucci.UI.Core` for better access to core functionalities. Enhanced code readability by adding comments to serialized fields and refactoring methods for dialog flow control, such as `StartOrContinueDialog`, `DisplayNextDialog`, and introducing `EnqueueDialogsFromCurrentCharacter` and `HandleDialogEnd` for efficient dialog management. Removed unused variables and methods for cleaner code.
    
    - Modified Dialog.cs to use more flexible data structures and updated namespace requirements, indicating a shift in handling dialog text and speaker images. Changed `_text` field from an array to a `List<string>` and `_image` field from `SpriteRenderer` to `Sprite` for improved flexibility and simplicity.
    
    - Cleaned up DefaultVolumeProfile.asset by removing unnecessary `fileID` entries, likely as part of optimization efforts.

--- Assets/Project/Scripts/System/Dialog System/Dialog.cs
+++ Assets/Project/Scripts/System/Dialog System/Dialog.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class Dialog
 {
     public string _name;
-    public SpriteRenderer _image;
-    [TextArea(5, 10)] public string[] _text;
+    public Sprite _image;
+    [TextArea(5, 10)] public List<string> _text;
 }
