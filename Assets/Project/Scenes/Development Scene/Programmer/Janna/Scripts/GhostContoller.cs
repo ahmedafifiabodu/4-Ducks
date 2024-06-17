800782fb3c9fd07b3aa8c669c5a6cca2befb6688
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-17 23:48:43 +0300

    Refactor and enhance game mechanics
    This commit introduces a series of significant changes aimed at refining the game's codebase and enhancing gameplay mechanics. Key modifications include:
    
    - Refactored Service Locator usage for cleaner code, introducing a `_serviceLocator` field for accessing services like `InputManager`.
    - Standardized logging and debugging by replacing `Debug.DrawRay` with `Logging.DrawRay` in `CatController` and `GhostController`.
    - Simplified and modernized code by employing expression-bodied members and inline out variable declarations in raycast calls.
    - Adjusted behaviors in `CatController` and `GhostController` for improved gameplay, including a new double jump logic for the cat and dashing, ascending, and gravity adjustments for the ghost.
    - Made serialization and component adjustments, notably in `GhostController`, for better movement customization and removed outdated save/load game data mechanisms.
    - Conducted general code cleanup for better readability and maintainability, including removing unused usings and comments.
    - Tweaked animations and physics for a more fluid player experience.
    - Introduced various minor adjustments for better game logic and structure, including new checks for player states and improved variable naming.
    - Modified Unity assets and metadata for Level 1 - Room 2, including changes to MonoBehaviour, PrefabInstances, Transforms, and GameObjects, and the creation of new dialog assets.
    - Enhanced the Enemy Health System UI for dynamic interaction with the game's camera.
    
    These changes collectively aim to improve the game's code quality, player interaction, and overall gameplay experience.

--- Assets/Project/Scenes/Development Scene/Programmer/Janna/Scripts/GhostContoller.cs
+++ Assets/Project/Scenes/Development Scene/Programmer/Janna/Scripts/GhostContoller.cs
@@ -1,76 +1,74 @@
-using UnityEngine;
 using System.Collections;
-using UnityEngine.InputSystem;
-using System;
+using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
 {
     #region Parameters
 
-    private Action<InputAction.CallbackContext> _dashAction;
-    private Action<InputAction.CallbackContext> _startAscendAction;
-    private Action<InputAction.CallbackContext> _stopAscendAction;
+    [SerializeField] private bool canFly;
+    [SerializeField] private ParticleSystem _movingEffect;
 
     private Vector2 input;
     private bool isAscending = false;
     private bool canDash = true;
     private bool isDashing = false;
-    [SerializeField] private bool canFly;
 
-    [SerializeField] private ParticleSystem _movingEffect;
-
-    PlayerState GhostState;
+    private PlayerState GhostState;
 
     [Header("BroadCasting")]
-    // reference of two events 
+    // reference of two events
     [SerializeField] private UnityEvent _onAscend;
+
     [SerializeField] private UnityEvent _onDescend;
 
     [Header("Movement")]
     [SerializeField] private float Speed = 20f;
+
     [SerializeField] private float rotationSpeed = 10f;
 
     [Header("Dashing")]
     [SerializeField] private float dashSpeed = 100f;
+
     [SerializeField] private float dashDistance = 10f;
     private float dashTime;
 
     [Header("Ascending")]
     [SerializeField] private float ascendingSpeed = 40f;
-    private float ghostDistance = 0.1f;
+
     [SerializeField] private float gravity = -9.81f;
+
+    private readonly float ghostDistance = 0.1f;
     private float distanceToGround;
     private RaycastHit reachedDistance;
 
     [Header("Steps")]
     [SerializeField] private float startRay = 0.5f;
+
     [SerializeField] private float rayLength = 1f;
     [SerializeField] private float stepSmooth = 20f;
     [SerializeField] private float stepHeight = 1f;
     [SerializeField] private float maxClimbHeight = 0.5f;
-
-    private int RunAnimationId;
-    private RaycastHit ishit;
     [SerializeField] private LayerMask detectWall;
 
-    #endregion
-    protected override void Awake()
-    {
-        base.Awake();
-    }
+    private readonly int RunAnimationId;
+    private RaycastHit ishit;
+
+    #endregion Parameters
+
+    protected override void Awake() => base.Awake();
+
     protected override void Start()
     {
         base.Start();
-        _dashAction = context => Dash();
     }
+
     private void Update()
     {
         input = _inputManager.GhostActions.Move.ReadValue<Vector2>().normalized;
         if (input.magnitude > 0.1f)
-        {
             Move(input);
-        }
 
         if (canFly)
         {
@@ -155,9 +153,7 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
             if (ShouldStep(moveDirection))
-            {
                 Step(moveDirection);
-            }
             else
             {
                 Vector3 newVelocity = moveDirection * Speed;
@@ -166,31 +162,26 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
             }
         }
         else
-        {
             _animator.SetFloat(RunAnimationId, 0);
-        }
     }
 
     public bool ShouldStep(Vector3 moveDirection)
     {
         Vector3 rayOrigin = transform.position + Vector3.up * startRay;
         Vector3 rayDirection = moveDirection;
-        Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.blue);
 
-        RaycastHit hit;
-        if (Physics.Raycast(rayOrigin, rayDirection, out hit, rayLength))
+        Logging.DrawRay(rayOrigin, rayDirection * rayLength, Color.blue);
+
+        if (Physics.Raycast(rayOrigin, rayDirection, out RaycastHit hit, rayLength))
         {
             float heightDifference = hit.point.y - transform.position.y;
             Vector3 stepRayOrigin = rayOrigin + Vector3.up * stepHeight;
 
             if (heightDifference > 0.1f && heightDifference < stepHeight && heightDifference < maxClimbHeight)
-            {
-                if (!Physics.Raycast(stepRayOrigin, rayDirection, out hit, rayLength))
-                {
+                if (!Physics.Raycast(stepRayOrigin, rayDirection, out _, rayLength))
                     return true;
-                }
-            }
         }
+
         return false;
     }
 
@@ -198,10 +189,10 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
     {
         Vector3 rayOrigin = transform.position + Vector3.up * startRay;
         Vector3 rayDirection = moveDirection;
-        Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.blue);
 
-        RaycastHit hit;
-        if (Physics.Raycast(rayOrigin, rayDirection, out hit, rayLength))
+        Logging.DrawRay(rayOrigin, rayDirection * rayLength, Color.blue);
+
+        if (Physics.Raycast(rayOrigin, rayDirection, out RaycastHit hit, rayLength))
         {
             float heightDifference = hit.point.y - transform.position.y;
 
@@ -210,31 +201,27 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
                 Vector3 normal = hit.normal;
                 Vector3 projectedDirection = Vector3.ProjectOnPlane(rayDirection, normal);
 
-                Vector3 stepUpPosition = transform.position + projectedDirection.normalized * Speed * Time.deltaTime;
+                Vector3 stepUpPosition = transform.position + Speed * Time.deltaTime * projectedDirection.normalized;
                 stepUpPosition.y = hit.point.y + stepHeight;
 
                 rb.MovePosition(Vector3.Lerp(rb.position, stepUpPosition, stepSmooth * Time.deltaTime));
             }
             else
-            {
                 rb.AddForce(Vector3.up * gravity);
-            }
         }
         else
-        {
             rb.AddForce(Vector3.up * gravity);
-        }
     }
 
     public void Dash()
     {
         if (GhostState != PlayerState.Dashing)
             return;
+
         GhostState = PlayerState.Dashing;
+
         if (canDash && !isDashing)
-        {
             StartCoroutine(PerformDash());
-        }
     }
 
     public IEnumerator PerformDash()
@@ -243,9 +230,7 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
         isDashing = true;
 
         if (Physics.Raycast(transform.position, transform.forward, out ishit, dashDistance, detectWall))
-        {
             SetWallTrigger(ishit.collider, true);
-        }
 
         rb.velocity = transform.forward * dashSpeed;
         dashTime = dashDistance / dashSpeed;
@@ -256,20 +241,18 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
         isDashing = false;
 
         if (ishit.collider != null)
-        {
             SetWallTrigger(ishit.collider, false);
 
-        }
         canDash = true;
         GhostState = PlayerState.moving;
     }
+
     public void SetWallTrigger(Collider wallCollider, bool isTrigger)
     {
         if (wallCollider != null)
-        {
             wallCollider.isTrigger = isTrigger;
-        }
     }
+
     public void Ascend()
     {
         if (GhostState != PlayerState.Ascending)
@@ -277,6 +260,7 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
 
         rb.velocity = new Vector3(rb.velocity.x, ghostDistance * ascendingSpeed, rb.velocity.z);
     }
+
     public void ApplyGravity()
     {
         if (Physics.Raycast(transform.position, Vector3.down, out reachedDistance))
@@ -284,9 +268,7 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
             distanceToGround = reachedDistance.distance;
 
             if (distanceToGround > ghostDistance)
-            {
                 rb.useGravity = true;
-            }
             else
             {
                 rb.useGravity = false;
@@ -294,21 +276,8 @@ public class GhostController : PlayerController, IMove, IDash, IStep, IAscend
             }
         }
     }
-    private void MovingEffect()
-    {
-        _movingEffect.Play();
-    }
-    private void StoppingEffect()
-    {
-        _movingEffect.Stop();
-    }
-    public override void LoadGame(GameData _gameData)
-    {
-        transform.position = _gameData._playerPosition;
-    }
 
-    public override void SaveGame(GameData _gameData)
-    {
-        _gameData._playerPosition = transform.position;
-    }
+    private void MovingEffect() => _movingEffect.Play();
+
+    private void StoppingEffect() => _movingEffect.Stop();
 }
