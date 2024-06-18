40c2ce69bf471a916d3b94fd00266be169df1dd1
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-18 16:57:12 +0300

    - Update

--- Assets/Project/Scripts/System/UI System/UI Buttons Animation And Hover Effect.cs
+++ Assets/Project/Scripts/System/UI System/UI Buttons Animation And Hover Effect.cs
@@ -1,5 +1,4 @@
 using DG.Tweening;
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,59 +6,16 @@ using UnityEngine.EventSystems;
 public class UIButtonsAnimationAndHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private RectTransform buttonRectTransform; // Assign in inspector
-    [SerializeField] private TMP_Text buttonText; // Assign your TextMeshPro text component in inspector
     [SerializeField] private float animationDuration = 0.3f; // Duration of the animation
     [SerializeField] private Vector3 expandedScale = new(); // Target scale when expanded
-    [SerializeField] private Color outlineColor = Color.white; // Outline color
-    [SerializeField] private float outlineWidth = 0.1f; // Outline width
-
-    private Material originalMaterial; // To store the original material
-    private Material outlineMaterial; // Material with outline
 
     private void Start()
     {
         if (buttonRectTransform == null)
             buttonRectTransform = GetComponent<RectTransform>();
-
-        if (buttonText == null)
-            buttonText = GetComponentInChildren<TMP_Text>();
-
-        // Store the original material
-        originalMaterial = buttonText.fontMaterial;
-
-        // Create a new material instance for the outline
-        outlineMaterial = new Material(originalMaterial);
-        EnableOutline(outlineMaterial, true);
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        // Scale up animation
-        buttonRectTransform.DOScale(expandedScale, animationDuration).SetEase(Ease.InElastic);
+    public void OnPointerEnter(PointerEventData eventData) => buttonRectTransform.DOScale(expandedScale, animationDuration).SetEase(Ease.InElastic);
 
-        // Apply the outline material
-        buttonText.fontMaterial = outlineMaterial;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        // Scale down animation
-        buttonRectTransform.DOScale(Vector3.one, animationDuration).SetEase(Ease.OutElastic);
-
-        // Revert to the original material
-        buttonText.fontMaterial = originalMaterial;
-    }
-
-    private void EnableOutline(Material material, bool enable)
-    {
-        if (enable)
-        {
-            material.SetFloat(ShaderUtilities.ID_OutlineWidth, outlineWidth);
-            material.SetColor(ShaderUtilities.ID_OutlineColor, outlineColor);
-        }
-        else
-        {
-            material.SetFloat(ShaderUtilities.ID_OutlineWidth, 0);
-        }
-    }
+    public void OnPointerExit(PointerEventData eventData) => buttonRectTransform.DOScale(Vector3.one, animationDuration).SetEase(Ease.OutElastic);
 }
