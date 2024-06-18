7b07a4a9a951f8d7e712dc27f941eb35b11e0050
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-18 15:39:30 +0300

    - Added UI Element - Applying UI System

--- Assets/Project/Scripts/System/Scene System/Scene Management.cs
+++ Assets/Project/Scripts/System/Scene System/Scene Management.cs
@@ -7,9 +7,13 @@ public class SceneManagement : MonoBehaviour
     [SerializeField] private List<Level> levels = new();
 
     private int currentLevel = 1;
+
     private List<string> levelScenes;
+    private InputManager inputManager;
 
     private void Awake() => ServiceLocator.Instance.RegisterService(this, true);
+
+    private void Start() => inputManager = ServiceLocator.Instance.GetService<InputManager>();
 
     public void StartLevel(int _targetLevel = -1)
     {
@@ -26,6 +30,35 @@ public class SceneManagement : MonoBehaviour
             Logging.LogError("No scenes found for level " + currentLevel);
             currentLevel--;
 
+            return;
+        }
+
+        // Check if the current level is the first one and disable PauseActions if so
+        if (currentLevel == 1 && inputManager != null)
+            inputManager.PauseActions.Disable();
+
+        // Load the first scene in Single mode
+        SceneManager.LoadScene(levelScenes[0], LoadSceneMode.Single);
+
+        // Load the rest of the scenes in Additive mode
+        for (int i = 1; i < levelScenes.Count; i++)
+            SceneManager.LoadScene(levelScenes[i], LoadSceneMode.Additive);
+    }
+
+    public void StartFirstLevel()
+    {
+        currentLevel = 1; // Reset currentLevel to the first level
+
+        // Optionally, disable PauseActions if needed when starting the first level
+        if (inputManager != null)
+            inputManager.PauseActions.Disable();
+
+        // Get all scenes in the first level
+        levelScenes = GetLevelScenes(currentLevel);
+
+        if (levelScenes == null || levelScenes.Count == 0)
+        {
+            Logging.LogError("No scenes found for level " + currentLevel);
             return;
         }
 
