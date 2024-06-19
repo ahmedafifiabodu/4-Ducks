bb8f88f197f86a65dde77d1ebc003f9fc28e43c9
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-19 12:26:11 +0300

    Scene and script enhancements
    Updated "Level 1 - Room 1 (Environment & Lights).unity" with material adjustments, object renaming, and position changes for better scene visualization and performance. Refactored and improved C# scripts including RoomManager.cs, InputManager.cs, and SpawnSystem.cs for readability, gameplay mechanics adjustments, and checkpoint system enhancements. Added a new meta file for asset tracking and version control.

--- Assets/Project/Scripts/Managers/RoomManager.cs
+++ Assets/Project/Scripts/Managers/RoomManager.cs
@@ -5,5 +5,12 @@ public class RoomManager : MonoBehaviour
 {
     [SerializeField] private CinemachineBlenderSettings _cameraBlendSettings;
 
-    private void Start() => ServiceLocator.Instance.GetService<CameraInstance>().ChangeCustomBlend(_cameraBlendSettings);
+    ServiceLocator serviceLocator;
+
+    private void Start()
+    {
+        ServiceLocator.Instance.GetService<CameraInstance>().ChangeCustomBlend(_cameraBlendSettings);
+
+
+    }
 }
