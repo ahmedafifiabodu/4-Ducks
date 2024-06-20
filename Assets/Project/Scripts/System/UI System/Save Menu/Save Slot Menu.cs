a4b461314479970cad6b93c3deed8a458270c9db
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-06-20 04:03:29 +0300

    - Update

--- Assets/Project/Scripts/System/UI System/Save Menu/Save Slot Menu.cs
+++ Assets/Project/Scripts/System/UI System/Save Menu/Save Slot Menu.cs
@@ -85,7 +85,8 @@ public class SaveSlotMenu : MonoBehaviour
             () => ActivatedMenu(_isGameLoading)
             );
 
-        eventSystem.firstSelectedGameObject = _gameObject;
+        if (eventSystem != null)
+            eventSystem.firstSelectedGameObject = _gameObject;
     }
 
     private void SaveGameAndLoadScene()
